2839d4c98f79a6fc1c3964ee0f70beb448b27f83
cjom <55603581+cjom@users.noreply.github.com>
2026-03-09 14:04:16 +0000

    Fix install2emmc script

--- package/tools/install2emmc/install2emmc.mk
+++ package/tools/install2emmc/install2emmc.mk
@@ -10,8 +10,7 @@ INSTALL2EMMC_PATH = $(BR2_EXTERNAL_RESCUE_PATH)/package/tools/install2emmc/src
 
 define INSTALL2EMMC_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/bin/
-	install -m 0755 $(INSTALL2EMMC_PATH)/install2emmc \
-		$(TARGET_DIR)/usr/bin/
+	install -m 0755 $(INSTALL2EMMC_PATH)/install2emmc $(TARGET_DIR)/usr/bin/
 endef
 
 $(eval $(generic-package))
